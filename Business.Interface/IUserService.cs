81379af32351cf087e47cc468950a29d607d6d04
TunahanKaracoban <krctunahan05@gmail.com>
2022-05-09 22:22:18 +0300

    first commit

---
+++ Business.Interface/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using Business.Entity.Dto;
+using Business.Entity.IBase;
+using Business.Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interface
+{
+    public interface IUserService:IGenericService<User,DtoUser>
+    {
+        IResponse<DtoUserToken> Login(DtoLogin login);
+    }
+}
